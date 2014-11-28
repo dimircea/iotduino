1ac90153ee1796aabda94857301fb5c89049a70a
dimircea <dimircea@gmail.com>
2014-11-28 14:41:53 +0100

    UPDATE: add iotduino release build.

---
+++ iotduino/build/Release/.deps/Release/iotduino.node.d
@@ -0,0 +1,1 @@
+cmd_Release/iotduino.node := rm -rf "Release/iotduino.node" && cp -af "Release/obj.target/iotduino.node" "Release/iotduino.node"
---
+++ iotduino/build/Release/.deps/Release/obj.target/iotduino/src/iotduino.o.d
@@ -0,0 +1,29 @@
+cmd_Release/obj.target/iotduino/src/iotduino.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/ubuntu/.node-gyp/0.10.28/src -I/home/ubuntu/.node-gyp/0.10.28/deps/uv/include -I/home/ubuntu/.node-gyp/0.10.28/deps/v8/include -I../src  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/iotduino/src/iotduino.o.d.raw  -c -o Release/obj.target/iotduino/src/iotduino.o ../src/iotduino.cc
+Release/obj.target/iotduino/src/iotduino.o: ../src/iotduino.cc \
+ /home/ubuntu/.node-gyp/0.10.28/src/node.h \
+ /home/ubuntu/.node-gyp/0.10.28/deps/uv/include/uv.h \
+ /home/ubuntu/.node-gyp/0.10.28/deps/uv/include/uv-private/uv-unix.h \
+ /home/ubuntu/.node-gyp/0.10.28/deps/uv/include/uv-private/ngx-queue.h \
+ /home/ubuntu/.node-gyp/0.10.28/deps/uv/include/uv-private/uv-linux.h \
+ /home/ubuntu/.node-gyp/0.10.28/deps/v8/include/v8.h \
+ /home/ubuntu/.node-gyp/0.10.28/deps/v8/include/v8stdint.h \
+ /home/ubuntu/.node-gyp/0.10.28/src/node_object_wrap.h \
+ /home/ubuntu/.node-gyp/0.10.28/src/node.h \
+ ../src/../../common/src/Duino.h ../src/../../common/src/PinMode.h \
+ ../src/../../common/src/PinState.h ../src/../../common/src/PinSource.h \
+ ../src/../../common/src/Pins.h
+../src/iotduino.cc:
+/home/ubuntu/.node-gyp/0.10.28/src/node.h:
+/home/ubuntu/.node-gyp/0.10.28/deps/uv/include/uv.h:
+/home/ubuntu/.node-gyp/0.10.28/deps/uv/include/uv-private/uv-unix.h:
+/home/ubuntu/.node-gyp/0.10.28/deps/uv/include/uv-private/ngx-queue.h:
+/home/ubuntu/.node-gyp/0.10.28/deps/uv/include/uv-private/uv-linux.h:
+/home/ubuntu/.node-gyp/0.10.28/deps/v8/include/v8.h:
+/home/ubuntu/.node-gyp/0.10.28/deps/v8/include/v8stdint.h:
+/home/ubuntu/.node-gyp/0.10.28/src/node_object_wrap.h:
+/home/ubuntu/.node-gyp/0.10.28/src/node.h:
+../src/../../common/src/Duino.h:
+../src/../../common/src/PinMode.h:
+../src/../../common/src/PinState.h:
+../src/../../common/src/PinSource.h:
+../src/../../common/src/Pins.h:
